a7896feb228e39c6a58f0bbd044a78317c6ee212
Niklas Tasler <niklastasler@gmail.com>
2025-09-23 22:23:28 +0200

    feat: glance ci_config

--- ci_config.nix
+++ ci_config.nix
@@ -225,6 +225,50 @@ in {
         };
       };
 
+      glance = {
+        enable = true;
+        settings.pages.home = {
+          columns.start = {
+            rank = 500;
+            size = "small";
+            widgets = [
+              {
+                type = "server-stats";
+                servers = [
+                  {
+                    type = "local";
+                    name = "Server";
+                  }
+                ];
+              }
+              {
+                type = "reddit";
+                subreddit = "selfhosted";
+                collapse-after = 3;
+              }
+            ];
+          };
+          columns.end = {
+            rank = 1500;
+            size = "small";
+            widgets = [
+              {
+                type = "clock";
+                time-format = "24h";
+                date-format = "d MMMM yyyy";
+                show-seconds = true;
+                show-timezone = true;
+                timezone = config.nps.defaultTz;
+              }
+              {
+                type = "calendar";
+                first-day-of-week = "monday";
+              }
+            ];
+          };
+        };
+      };
+
       guacamole = {
         enable = true;
         userMappingXml = pkgs.writeText "user-mapping.xml" ''
